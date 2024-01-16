b8d678f9a49e1b828ef55ec6cc318713a8962421
hieucannguyen <hieucanguyen@yahoo.com>
2024-01-15 20:11:39 -0600

    update strategy

--- bot.py
+++ bot.py
@@ -18,22 +18,25 @@ client = commands.Bot(command_prefix=prefix, intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-
+# create chart command
 @client.command(name="chart")
 async def create_chart(ctx):
+    # asks user for a stock and returns a 1 day chart for specified stock ticker
     await ctx.send('Enter a stock: ')
 
+    # checks same user and channel
     def check(msg):
         return msg.author == ctx.author and msg.channel == ctx.channel
+    # ensure correct stock ticker
     try:
         msg = await client.wait_for("message", check=check, timeout=30)
         stock = trade.trade()
         stock.create_charts(f'{msg.content}')  # 30 seconds to reply
-        await ctx.channel.send(file=discord.File('chart.png'))
+        await ctx.channel.send(file=discord.File('chart.png')) # send chart image
     except:
         await ctx.send("Not a real stock.")
 
-
+# End of day stock information
 @client.command(name="EOD")
 async def create_chart(ctx):
     await ctx.send('Enter a stock: ')
@@ -48,7 +51,8 @@ async def create_chart(ctx):
     except:
         await ctx.send("Not a real stock.")
 
-
+# compare 2 different stocks
+# displays 2 charts side by side
 @client.command(name="compare")
 async def create_chart(ctx):
     await ctx.send('Enter a stock: ')
@@ -65,7 +69,7 @@ async def create_chart(ctx):
     except:
         await ctx.send("At least one of the stocks is not real")
 
-
+# ultilize bollinger strategy on a stock
 @client.command(name="bollinger")
 async def bollinger_strategy(ctx):
     stock = trade.trade()
@@ -74,7 +78,7 @@ async def bollinger_strategy(ctx):
         return msg.author == ctx.author and msg.channel == ctx.channel
     await ctx.send('Enter a stock: ')
     msg = await client.wait_for("message", check=check, timeout=30)
-    await ctx.send('Available intervals [1d, 1mo, 1y, 2y, max]\nEnter an interval: ')
+    await ctx.send('Available intervals [1d, 1mo, 1y, 2y, max]\nEnter an interval: ') # specify interval
     interval = await client.wait_for("message", check=check, timeout=30)
     bollinger = stock.bollinger(f'{msg.content}', f'{interval.content}')
     try:
@@ -85,4 +89,5 @@ async def bollinger_strategy(ctx):
     except:
         await ctx.channel.send(bollinger)
 
+# run the bot
 client.run(TOKEN)
--- trade.py
+++ trade.py
@@ -6,14 +6,16 @@ import numpy as np
 
 class trade:
     def __init__(self):
-        #self.stockName = stockName
         self.name = 'default'
 
     def create_charts(self, stockName):
-        data = yf.download(tickers=f'{stockName}', period='1d', interval='1m')
+        # download stock data from yfinance into a pandas dataframe
+        data = yf.download(tickers=f'{stockName}', period='1d', interval='5m')
+        # customize style on matplotlib finance
         customstyle = mpf.make_mpf_style(base_mpf_style='yahoo',
                                          y_on_right=False,
                                          facecolor='w')
+        # plot stock
         mpf.plot(data,
                  type='candle',
                  title=f'${stockName} price',
@@ -26,16 +28,20 @@ class trade:
                  savefig='chart.png')
 
     def compare_stocks(self, stockName1, stockName2):
+        # download data for 2 stocks
         stock1 = yf.download(f'{stockName1}', period='1d', interval='1m')
         stock2 = yf.download(f'{stockName2}', period='1d', interval='1m')
         customstyle = mpf.make_mpf_style(base_mpf_style='yahoo',
                                          y_on_right=False,
                                          facecolor='w')
+        # additional charts
+        # bar charts for stock volume
         adds = [mpf.make_addplot(stock1.Volume, type='bar', panel=1, ylabel='Volume', y_on_right=False),
                 mpf.make_addplot(stock2, type='candle', panel=2,
                                  ylabel=f'\${stockName2} (\$USD)'),
                 mpf.make_addplot(stock2.Volume, type='bar', panel=3, ylabel='Volume', y_on_right=False)]
 
+        # plot
         mpf.plot(stock1,
                  type='candle',
                  title=f'\${stockName1} vs \${stockName2}',
@@ -50,9 +56,11 @@ class trade:
         data = yf.download(tickers=f'{stockName}', period='1d')
         df = pd.DataFrame(data)
         adj = df['Adj Close'][0]
+        # return EOD of stock
         return f'EOD update on ${stockName}\n-Date: {df.index[0].date()}\n-Open: {df.Open[0]}\n-High: {df.High[0]}\n-Low: {df.Low[0]}\n-Close: {df.Close[0]}\n-Adj Close: {adj}\n-Volume: {df.Volume[0]}'
 
     def calcBollAndRsi(data):
+        # add additional columns necessary for bollinger bands strategy
         data['Percent Change'] = data['Close'].pct_change()
         data['Buy&Hold returns'] = (1+data['Percent Change']).cumprod()
         data['Middle Band'] = data['Close'].rolling(window=21).mean()
@@ -70,9 +78,11 @@ class trade:
         data['RSI'] = data['RS'].apply(lambda x: 100-(100/(x+1)))
 
     def bollingerBandRsiStrategy(data):
+        # signal conditions
         conditions = [(data['Close'] < data['Lower Band']) & (data['RSI'] < 30),
                       (data['Close'] > data['Upper Band']) & (data['RSI'] > 70)]
         choices = ['Buy', 'Sell']
+        # buy or sell depending on which condition
         data['Signals'] = np.select(conditions, choices)
         data['Signals'] = data['Signals'].shift(1)
         data.dropna(inplace=True)
@@ -81,12 +91,15 @@ class trade:
         signal = []
         position = False
 
+        # go through stock dataframe and buy or sell stock depending on the postion and signal
         for i in range(len(data)):
+            # meaning we have not bought yet and signal indicates buy
             if not position and data['Signals'][i] == 'Buy':
                 buyDate.append(data.index[i])
                 buyPrice.append(data.Open[i])
                 sellPrice.append(np.nan)
                 position = True
+            # we have bought and signal indicates sell 
             elif position and data['Signals'][i] == 'Sell':
                 sellDate.append(data.index[i])
                 sellPrice.append(data.Open[i])
@@ -95,6 +108,7 @@ class trade:
             else:
                 buyPrice.append(np.nan)
                 sellPrice.append(np.nan)
+            # continously track our position to calculate cumulative returns 
             if not position:
                 signal.append(0)
             else:
@@ -103,13 +117,14 @@ class trade:
         return buyDate, sellDate, buyPrice, sellPrice, signal
 
     def calcProfits(data, buyDate, sellDate):
+        # calcuate cumulative returns based on the strategy
         buys = data.loc[buyDate].Open
         sells = data.loc[sellDate].Open
-        profits = (pd.Series([(sell - buy) / buy for sell,
-                   buy in zip(sells, buys)])+1).prod()-1
+        profits = (pd.Series([(sell - buy) / buy for sell, buy in zip(sells, buys)])+1).prod()-1
         return profits
 
     def bollinger(self, stockName, interval):
+        # check valid stock ticker
         try:
             data = yf.Ticker(f'{stockName}').history(period='1y')
             if data.empty:
@@ -117,6 +132,7 @@ class trade:
         except:
             return 'Not a real stock'
 
+        # get data based on interval
         if interval == '1d':
             df = yf.download(tickers=f'{stockName}',
                              period='1d', interval='1m')
@@ -143,17 +159,18 @@ class trade:
         customstyle = mpf.make_mpf_style(base_mpf_style='yahoo',
                                          y_on_right=False,
                                          facecolor='w')
-
+        # implement bollinger bands strategy
         trade.calcBollAndRsi(data)
-        buyDate, sellDate, buyPrice, sellPrice, signal = trade.bollingerBandRsiStrategy(
-            data)
+        buyDate, sellDate, buyPrice, sellPrice, signal = trade.bollingerBandRsiStrategy(data)
 
+        # find profits
         data['Position'] = signal
         data['Strategy returns'] = (
             1 + data['Position'] * data['Percent Change']).cumprod()
 
         profits = trade.calcProfits(data, buyDate, sellDate) * 100
 
+        # visualize upper and lower bands alongs with performance
         upper = np.asarray(data['Upper Band'])
         lower = np.asarray(data['Lower Band'])
         buyAndHold = np.asarray(data['Buy&Hold returns'])
@@ -168,6 +185,7 @@ class trade:
                                 type='line', panel=1, ylabel='Performance', color='red'),
                mpf.make_addplot(stratReturns, type='line', panel=1, ylabel='Performance', color='green')]
 
+        # plot
         mpf.plot(data,
                  type='candle',
                  title=f'${stockName} price',
@@ -179,6 +197,6 @@ class trade:
                  style=customstyle,
                  savefig='chart.png')
 
-        returns = str(
-            round(profits, 2))
+        # return profits
+        returns = str(round(profits, 2))
         return f'Legend for performance graph:\nGreen: Bollinger strategy\nRed: (default) Buy and hold strategy\nCumulative returns: {returns}%'
